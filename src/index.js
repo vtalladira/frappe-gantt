1962520a42bdb651de4f98183d720e2581e6dab7
vtalladira <56788786+vtalladira@users.noreply.github.com>
2023-06-21 16:27:20 +0930

    Update index.js

--- src/index.js
+++ src/index.js
@@ -106,6 +106,10 @@ export default class Gantt {
             // cache index
             task._index = i;
 
+            if (typeof task.row_id === 'number') {
+                task._index = task.row_id;
+            }
+
             // invalid dates
             if (!task.start && !task.end) {
                 const today = date_utils.today();
@@ -325,22 +329,28 @@ export default class Gantt {
     }
 
     make_grid_rows() {
+        let counter_rows = 0;
+        const distinctRows = [...new Set(this.tasks.map(x => x.row_id))];
+        for (let row of distinctRows){
+            counter_rows = counter_rows + 1;
+        }
+
         const rows_layer = createSVG('g', { append_to: this.layers.grid });
         const lines_layer = createSVG('g', { append_to: this.layers.grid });
-
         const row_width = this.dates.length * this.options.column_width;
         const row_height = this.options.bar_height + this.options.padding;
 
         let row_y = this.options.header_height + this.options.padding / 2;
 
-        for (let task of this.tasks) {
+        for (let row of distinctRows) {
+
             createSVG('rect', {
                 x: 0,
                 y: row_y,
                 width: row_width,
                 height: row_height,
                 class: 'grid-row',
-                append_to: rows_layer,
+                append_to: rows_layer
             });
 
             createSVG('line', {
@@ -349,7 +359,7 @@ export default class Gantt {
                 x2: row_width,
                 y2: row_y + row_height,
                 class: 'row-line',
-                append_to: lines_layer,
+                append_to: lines_layer
             });
 
             row_y += this.options.bar_height + this.options.padding;
